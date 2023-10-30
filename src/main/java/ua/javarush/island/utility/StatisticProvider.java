f4ffd0ef2dab1e4b309c2324c0a73f5a96e27df3
Denis Saranyuk <DSaranyuk@Service-Team.biz>
2023-10-30 17:37:21 +0200

    added ÑІефешіешÑStatisticProvider

--- src/main/java/ua/javarush/island/utility/StatisticProvider.java
+++ src/main/java/ua/javarush/island/utility/StatisticProvider.java
@@ -9,7 +9,7 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class StatisticProvider {
-    private StatisticProvider(){
+    private StatisticProvider() {
 
     }
 
@@ -21,31 +21,65 @@ public class StatisticProvider {
         return StatisticProvider.LazyHolder.INSTANCE;
     }
 
-    public void printStatisticArea(Area area) { //TODO need refactoring
+    public void printStatisticArea(Area area) {
+        printLocationsStatistic(area);
+        printAreaStatistic(area);
+    }
+
+    private void printAreaStatistic(Area area){
         Map<String, Integer> map = new TreeMap<>();
+        Location[][] locations = area.getLocations();
+        for (int i = 0; i < locations.length; i++) {
+            for (int j = 0; j < locations[0].length; j++) {
+                Map<Class<? extends Entity>, List<Entity>> entities = locations[i][j].getEntities();
+                for (Map.Entry<Class<? extends Entity>, List<Entity>> entry : entities.entrySet()) {
+                    String key = entry.getKey().getSimpleName();
+                    int value = entry.getValue().size();
+                    if (!map.containsKey(key)) {
+                        map.put(key, value);
+                    } else {
+                        map.put(key, map.get(key) + value);
+                    }
+                }
+            }
+        }
+        System.out.println("*".repeat(42));
+        for (Map.Entry<String, Integer> pair : map.entrySet()) {
+            System.out.println(pair.getKey() + " " + pair.getValue());
+        }
+    }
+
+    private void printLocationsStatistic(Area area) {
+        System.out.println("*".repeat(42));
         Map<String, StringBuilder> mapSb = new TreeMap<>();
         Location[][] locations = area.getLocations();
         for (int i = 0; i < locations.length; i++) {
             for (int j = 0; j < locations[0].length; j++) {
-                if (!mapSb.containsKey("!loc")) {
-                    mapSb.put("!loc", new StringBuilder("------ " + i + "," + j + " ------"));
+                Map<Class<? extends Entity>, List<Entity>> entities = locations[i][j].getEntities();
+                /*if (!mapSb.containsKey("!loc")) {
+                    mapSb.put("!loc", new StringBuilder("|----- " + i + "," + j + " -----|"));
                 } else {
-                    mapSb.put("!loc", mapSb.get("!loc").append(" ").append("------ ").append(i).append(",").append(j).append(" ------"));
+                    mapSb.put("!loc", mapSb.get("!loc").append(" ").append("|----- ").append(i).append(",").append(j).append(" -----|"));
+                }*/
+                if (!mapSb.containsKey("!loc")) {
+                    mapSb.put("!loc", new StringBuilder("|- " + i + "," + j + " -|"));
+                } else {
+                    mapSb.put("!loc", mapSb.get("!loc").append(" ").append("|- ").append(i).append(",").append(j).append(" -|"));
                 }
                 int length = mapSb.get("!loc").length();
-                Map<Class<? extends Entity>, List<Entity>> entities = locations[i][j].getEntities();
                 for (Map.Entry<Class<? extends Entity>, List<Entity>> entry : entities.entrySet()) {
-                    String key = entry.getKey().getSimpleName();
+                    //String key = entry.getKey().getSimpleName();
+                    String key = null;
+                    try {
+                        key = (String) entry.getKey().getField("icon").get(entry.getValue().get(0));
+                    } catch (IllegalAccessException | NoSuchFieldException e) {
+                        throw new RuntimeException(e);
+                    }
                     int value = entry.getValue().size();
                     if (!mapSb.containsKey(key)) {
                         mapSb.put(key, new StringBuilder(key + " " + value));
                     } else {
                         mapSb.put(key, mapSb.get(key).append(" ").append(key).append(" ").append(value));
-                    }
-                    if (!map.containsKey(key)) {
-                        map.put(key, value);
-                    } else {
-                        map.put(key, map.get(key) + value);
                     }
                     while (mapSb.get(key).length() < length) {
                         mapSb.put(key, mapSb.get(key).append(" "));
@@ -54,10 +88,6 @@ public class StatisticProvider {
             }
             mapSb.forEach((k, v) -> System.out.println(v));
             mapSb.clear();
-        }
-        System.out.println("*".repeat(42));
-        for (Map.Entry<String, Integer> pair : map.entrySet()) {
-            System.out.println(pair.getKey() + " " + pair.getValue());
         }
     }
 
