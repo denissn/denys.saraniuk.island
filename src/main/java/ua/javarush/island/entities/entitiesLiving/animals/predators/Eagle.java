c62fdf21501ca14bc2818443bf614343add95574
Denis Saranyuk <DSaranyuk@Service-Team.biz>
2023-10-30 14:35:30 +0200

    added іEagle

---
+++ src/main/java/ua/javarush/island/entities/entitiesLiving/animals/predators/Eagle.java
@@ -0,0 +1,7 @@
+package ua.javarush.island.entities.entitiesLiving.animals.predators;
+
+import ua.javarush.island.entities.abstractions.annotations.Config;
+
+@Config(filePath = "src/main/resources/config/entities/entitiesLiving/animals/predators/eagle.json")
+public class Eagle extends Predator {
+}
